9f2e5c9f806fca2cbda974258bca85df4e8ef19d
Erick <eaguilar4722@uta.edu.ec>
2025-05-13 15:32:54 -0500

    Añadida la clase Aguilar perteneciente a Erick Aguilar

---
+++ trabajoEquipo/src/trabajoequipo/Colaboradores/ClaseAguilar.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajoequipo.Colaboradores;
+
+/**
+ *
+ * @author User
+ */
+public class ClaseAguilar {
+    int nota;
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+    
+    
+}
