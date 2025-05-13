8ed70dfbfac63dce753b228845dad30a8a069dd0
Gabriel Llerena <gabriel0llerena@gmail.com>
2025-05-13 15:25:49 -0500

    Nuevos cambios

--- trabajoEquipo/src/trabajoequipo/ClaseLlerena.java
+++ trabajoEquipo/src/trabajoequipo/ClaseLlerena.java
@@ -4,7 +4,7 @@ package trabajoequipo;
  *
  * @author gabri
  */
-public class ClaseLlerena {
+public class ClaseLlerena { 
     
     public static void main(String[] args) {
         System.out.println("Hola Mundo, Soy Gabriel Llerena");
