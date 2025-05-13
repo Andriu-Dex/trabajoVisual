e3bb763ad5d69e72b49427e76f5c3d0e2cb5d3ef
NIXONHS <nixon2000paul@gmail.com>
2025-05-13 15:06:09 -0500

    Agregue Informacion

---
+++ trabajoEquipo/src/trabajoequipo/Colaboradores/ClaseHurtado.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajoequipo.Colaboradores;
+
+/**
+ *
+ * @author nixon
+ */
+public class ClaseHurtado {
+    public static void main(String[] args) {
+        System.out.println("Nixon Paul y estudio la carrera de Software en la Universidad Tecnica de Ambato");
+    }
+}
